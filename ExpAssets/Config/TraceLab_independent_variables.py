b9486c5ac52621db84c6983c6e8602813d53f450
Austin Hurst <mynameisaustinhurst@gmail.com>
2023-02-06 12:50:52 -0400

    Change repeated shape

--- ExpAssets/Config/TraceLab_independent_variables.py
+++ ExpAssets/Config/TraceLab_independent_variables.py
@@ -9,4 +9,4 @@ TraceLab_ind_vars = IndependentVariableSet()
 # Define project variables and variable types
 
 TraceLab_ind_vars.add_variable("animate_time", int, [500, 1000, 1500, 2000, 2500])
-TraceLab_ind_vars.add_variable("figure_name", str, ["random", "template_1477090164.31"])
+TraceLab_ind_vars.add_variable("figure_name", str, ["random", "template_1477106073.55"])
